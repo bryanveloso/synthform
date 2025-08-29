adb713a8f1545933207b258060a8b0f77b2c8404
Bryan Veloso <bryan@velo.so>
2025-08-29 10:54:02 -0700

    Initial setup for tanstack-query.

--- apps/overlays/src/main.tsx
+++ apps/overlays/src/main.tsx
@@ -4,7 +4,7 @@ import { useGSAP } from '@gsap/react'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
@@ -15,6 +15,8 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient()
+
 gsap.registerPlugin(Flip)
 gsap.registerPlugin(useGSAP)
 
@@ -23,7 +25,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   )
 }
