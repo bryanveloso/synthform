d411bde758f12198f6d0322fdf43b92969b4cc42
Bryan Veloso <bryan@velo.so>
2025-09-15 16:11:15 -0700

    Integrate the new status widget.

--- apps/overlays/src/routes/coworking.tsx
+++ apps/overlays/src/routes/coworking.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useRef } from 'react'
-import { createFileRoute } from '@tanstack/react-router'
 import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+import { createFileRoute } from '@tanstack/react-router'
 
 import { Canvas } from '@/components/ui/canvas'
 import { Circle, Frame } from '@/components/ui/window'
 import { LimitBreak } from '@/components/coworking/limitbreak'
 import { Timeline } from '@/components/coworking/timeline'
+import { Status } from '@/components/coworking/status'
 import { animateOverlayEntrance } from '@/lib/animations'
 
 export const Route = createFileRoute('/coworking')({
@@ -46,23 +47,7 @@ function RouteComponent() {
 
           <div className="flex flex-col gap-3 self-end">
             {/* !status */}
-            <div className="relative flex rounded-lg bg-gradient-to-b shadow-xl/50">
-              <div className="absolute top-0 left-0 h-full w-full rounded-lg inset-ring-2 inset-ring-white/10"></div>
-
-              <div className="from-shark-840 to-shark-880 flex items-center justify-center rounded-l-lg bg-gradient-to-b pr-4 pl-4.5">
-                <div className="outline-shark-920 from-lime to-lime/50 size-3.5 rounded-full bg-radial-[at_50%_25%] outline-4"></div>
-              </div>
-              <div className="from-shark-880 to-shark-920 text-shark-240 flex-1 rounded-r-lg bg-gradient-to-b p-3 font-sans text-shadow-sm/50">
-                Bryan is currently <span className="text-lime font-bold">online</span>.
-              </div>
-
-              {/* <div className="from-shark-840 to-shark-880 flex items-center justify-center rounded-l-lg bg-gradient-to-b pr-4 pl-4.5">
-                <div className="outline-shark-920 from-marigold to-marigold/50 size-3.5 rounded-full bg-radial-[at_50%_25%] outline-4"></div>
-              </div>
-              <div className="from-shark-880 to-shark-920 text-shark-240 flex-1 rounded-r-lg bg-gradient-to-b p-3 font-sans text-shadow-sm/50">
-                Bryan is currently <span className="text-marigold font-bold">away</span>.
-              </div> */}
-            </div>
+            <Status />
 
             {/* !hire */}
             <div className="from-shark-880 to-shark-920 rounded-lg bg-gradient-to-b p-3 shadow-xl/50 inset-ring-2 inset-ring-white/10">
