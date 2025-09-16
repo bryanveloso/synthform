ec7107066760df1a927ff26becbc7bc6f7ecd5ef
Bryan Veloso <bryan@velo.so>
2025-09-15 19:40:33 -0700

    `overflow: hidden` on the Canvas.

--- apps/overlays/src/components/ui/canvas.tsx
+++ apps/overlays/src/components/ui/canvas.tsx
@@ -1,5 +1,7 @@
 import type { FC, PropsWithChildren } from "react";
 
 export const Canvas: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="relative aspect-video h-[1080px] w-[1920px]">{children}</div>
+  return (
+    <div className="relative aspect-video h-[1080px] w-[1920px] overflow-hidden">{children}</div>
+  )
 };
